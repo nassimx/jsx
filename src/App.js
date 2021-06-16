bb137b1d2e533ef0d7f8e2d460b3130b8eccde13
nassimx <mr.nassimfr@gmail.com>
2021-06-16 22:57:30 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import img2 from '../src/img2.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
+        <h1 className="title">Nassim</h1>
+        <br />
+        <img src="/img1.jpg" alt="" />
+        <br />
+        <img src={img2} alt="" />
+      </div>
+
+      <iframe width={853} height={480} src="https://www.youtube.com/embed/HMZNWM-r4ic" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+
     </div>
   );
 }
